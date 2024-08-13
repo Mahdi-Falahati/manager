ef5efe641406eaface03e3441dd0b476d2ad249b
Mahdi-Falahati <falahatimahdi01@gmail.com>
2024-08-13 06:11:19 -0700

    make menubar

---
+++ components/modules/Menu.jsx
@@ -0,0 +1,11 @@
+import BurgerMenu from "@/elements/BurgerMenu";
+import NavBar from "@/elements/NavBar";
+
+export default function Menu() {
+  return (
+    <nav>
+      <NavBar />
+      <BurgerMenu />
+    </nav>
+  );
+}
