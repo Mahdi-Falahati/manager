b94b8e52a1f3f19ed0fe629f3288a9238ac6a69f
Mahdi-Falahati <falahatimahdi01@gmail.com>
2024-08-13 02:49:42 -0700

    check authentication routes

--- pages/sign-in.js
+++ pages/sign-in.js
@@ -1,5 +1,17 @@
 import SignIng from "@/templates/SignIng";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 export default function Index() {
+  const { status } = useSession();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (status === "authenticated") {
+      router.replace("/");
+    }
+  }, [status]);
+
   return <SignIng />;
 }
--- pages/sign-up.js
+++ pages/sign-up.js
@@ -1,5 +1,16 @@
 import SignUp from "@/templates/SignUp";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 export default function Index() {
-  return <SignUp/>
+  const { status } = useSession();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (status === "authenticated") {
+      router.replace("/");
+    }
+  }, [status]);
+  return <SignUp />;
 }
