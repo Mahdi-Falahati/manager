34101fc87d4427114d4f7ba6d04c51d45802321f
Mahdi-Falahati <falahatimahdi01@gmail.com>
2024-08-13 02:31:09 -0700

    write sign in route

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,44 @@
+import User from "@/model/User";
+import { ValidateEmail, VerifyPassword } from "@/utils/auth";
+import connectDB from "@/utils/connectDB";
+import NextAuth from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+
+const authOptions = {
+  session: { strategy: "jwt" },
+  providers: [
+    Credentials({
+      async authorize(credentials, req) {
+        try {
+          await connectDB();
+        } catch (error) {
+          throw new Error("Something is worng in the server...");
+        }
+
+        const { email, password } = credentials;
+        const isValid = await ValidateEmail(email);
+
+        if (!email || !password || !isValid) {
+          throw new Error("Email or password is inValid...");
+        }
+
+        const user = await User.findOne({ email });
+        if (!user) {
+          throw new Error("You are not registered...");
+        }
+
+        const passwordValidation = await VerifyPassword(
+          user.password,
+          password
+        );
+        if (passwordValidation) {
+          throw new Error("Email or password incorrect..");
+        }
+
+        return { email };
+      },
+    }),
+  ],
+};
+
+export default NextAuth(authOptions);
