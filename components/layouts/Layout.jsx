ce3e5654c1d2bf4440ab9099483036ef2dd51f8d
Mahdi-Falahati <falahatimahdi01@gmail.com>
2024-08-13 06:12:00 -0700

    add menu to layout

--- components/layouts/Layout.jsx
+++ components/layouts/Layout.jsx
@@ -1,14 +1,18 @@
 import { GiImp } from "react-icons/gi";
 import { BiSolidHourglass } from "react-icons/bi";
+import Menu from "@/modules/Menu";
 
 export default function Layout({ children }) {
   return (
     <>
-      <header className="container mx-auto mb-3">
-        <h1 className="italic font-bold text-2xl flex items-center text-gray-900 tracking-widest ">
-          Manager
-          <BiSolidHourglass className="text-green-600 text-xl ml-1" />
-        </h1>
+      <header className="container mx-auto mb-6 ">
+        <div className="flex justify-between items-center mx-2 flex-wrap mt-4">
+          <h1 className="italic font-bold text-2xl flex items-center text-gray-900 tracking-widest ">
+            Manager
+            <BiSolidHourglass className="text-green-600 text-xl ml-1" />
+          </h1>
+          <Menu />
+        </div>
       </header>
       <main className="container mx-auto min-h-[86vh]">{children}</main>
       <footer className="italic font-bold flex items-center justify-center tracking-widest text-gray-500 border-t border-t-gray-500 py-3">
