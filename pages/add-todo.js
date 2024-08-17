34892f4add9226c88fde7883eec8c093e162affd
Mahdi-Falahati <falahatimahdi01@gmail.com>
2024-08-17 07:03:23 -0700

    protected add todo route ( if user is not login redirect user to sign in page )

--- pages/add-todo.js
+++ pages/add-todo.js
@@ -1,5 +1,16 @@
 import AddTodo from "@/templates/AddTodo";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 export default function Index() {
+  const { status } = useSession();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (status !== "authenticated") {
+      router.replace("/sign-in");
+    }
+  }, [status]);
   return <AddTodo />;
 }
