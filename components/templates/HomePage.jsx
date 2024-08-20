b68943d995d4d3e2cc42eb433f8a792b31541bb1
Mahdi-Falahati <falahatimahdi01@gmail.com>
2024-08-20 07:57:56 -0700

    change set props for task component

--- components/templates/HomePage.jsx
+++ components/templates/HomePage.jsx
@@ -21,7 +21,12 @@ export default function HomePage() {
           Todo
         </h1>
         {todos.todo?.map((todo) => (
-          <Task next="In Progress" title={todo.title} key={todo._id} />
+          <Task
+            next="progress"
+            key={todo._id}
+            data={todo}
+            fetchData={fetchData}
+          />
         ))}
       </section>
       <section className="w-[300px] my-5 mx-2">
@@ -29,19 +34,26 @@ export default function HomePage() {
           In Progress
         </h1>
         {todos.progress?.map((todo) => (
-          <Task back="Todo" next="Review" title={todo.title} key={todo._id} />
+          <Task
+            back="todo"
+            next="review"
+            key={todo._id}
+            data={todo}
+            fetchData={fetchData}
+          />
         ))}
       </section>
       <section className="w-[300px] my-5 mx-2">
         <h1 className="font-bold bg-orange-500 rounded-md text-center py-1 mx-2 text-xl italic text-white tracking-widest">
           Review
         </h1>
-        {todos.progress?.map((todo) => (
+        {todos.review?.map((todo) => (
           <Task
-            back="In Progress"
-            next="Done"
-            title={todo.title}
+            back="progress"
+            next="done"
             key={todo._id}
+            data={todo}
+            fetchData={fetchData}
           />
         ))}
       </section>
@@ -49,8 +61,13 @@ export default function HomePage() {
         <h1 className="font-bold bg-green-700 rounded-md text-center py-1 mx-2 text-xl italic text-white tracking-widest">
           Done
         </h1>
-        {todos.progress?.map((todo) => (
-          <Task back="Review" title={todo.title} key={todo._id} />
+        {todos.done?.map((todo) => (
+          <Task
+            back="review"
+            data={todo}
+            key={todo._id}
+            fetchData={fetchData}
+          />
         ))}
       </section>
     </div>
