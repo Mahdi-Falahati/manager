e63585e853b2a0d812a226d170e52c2036bdeca0
Mahdi-Falahati <falahatimahdi01@gmail.com>
2024-08-20 07:57:30 -0700

    add handler for update status

--- components/modules/Task.jsx
+++ components/modules/Task.jsx
@@ -2,7 +2,23 @@ import { GiArcher } from "react-icons/gi";
 import { TiArrowForwardOutline } from "react-icons/ti";
 import { TiArrowBackOutline } from "react-icons/ti";
 
-export default function Task({ title, next, back }) {
+export default function Task({ data, next, back, fetchData }) {
+  const { _id, title } = data;
+
+  const changeStatus = async (status) => {
+    const req = await fetch("/api/todos", {
+      method: "PATCH",
+      body: JSON.stringify({ id: _id, status }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    const res = await req.json();
+
+    if (res.status === "success") {
+      fetchData();
+    }
+  };
+
   return (
     <section className="mx-2 mt-5 p-1 shadow-xl">
       <div className="flex justify-start items-start px-1">
@@ -13,13 +29,19 @@ export default function Task({ title, next, back }) {
       </div>
       <div className="flex justify-around items-center flex-wrap">
         {back && (
-          <button className={`${btnStyles} shadow-red-300`}>
+          <button
+            className={`${btnStyles} shadow-red-300`}
+            onClick={() => changeStatus(back)}
+          >
             <TiArrowBackOutline className="mr-1" />
             {back}
           </button>
         )}
         {next && (
-          <button className={`${btnStyles} shadow-green-300`}>
+          <button
+            className={`${btnStyles} shadow-green-300`}
+            onClick={() => changeStatus(next)}
+          >
             <TiArrowForwardOutline className="mr-1" />
             {next}
           </button>
@@ -30,4 +52,4 @@ export default function Task({ title, next, back }) {
 }
 
 const btnStyles =
-  "font-bold w-[130px] flex items-center justify-center italic text-gray-600 hover:text-gray-800 shadow-md p-2 rounded-xl tracking-wider my-3 mx-1";
+  "capitalize font-bold w-[120px] flex items-center justify-center italic text-gray-600 hover:text-gray-800 shadow-md p-2 rounded-xl tracking-wider my-3 mx-1";
