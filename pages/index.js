bc9aec4e45934c57109196fa937e2f570216d708
Mahdi-Falahati <falahatimahdi01@gmail.com>
2024-08-22 01:43:44 -0700

    import use effect

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import HomePage from "@/templates/HomePage";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 export default function Index() {
   const { status } = useSession();
