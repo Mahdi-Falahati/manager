e7649d4802a084e62cb90b5f82d0f4ef497927c7
Mahdi-Falahati <falahatimahdi01@gmail.com>
2024-08-22 02:30:34 -0700

    add loader for  showing todos before load

--- components/templates/HomePage.jsx
+++ components/templates/HomePage.jsx
@@ -1,3 +1,4 @@
+import Loading from "@/elements/Loading";
 import Task from "@/modules/Task";
 import { useState, useEffect } from "react";
 
@@ -16,60 +17,66 @@ export default function HomePage() {
 
   return (
     <div className="flex justify-around items-start flex-wrap">
-      <section className="w-[300px] my-5 mx-2">
-        <h1 className="font-bold bg-yellow-500 rounded-md text-center py-1 mx-2 text-xl italic text-white tracking-widest">
-          Todo
-        </h1>
-        {todos.todo?.map((todo) => (
-          <Task
-            next="progress"
-            key={todo._id}
-            data={todo}
-            fetchData={fetchData}
-          />
-        ))}
-      </section>
-      <section className="w-[300px] my-5 mx-2">
-        <h1 className="font-bold bg-purple-600 rounded-md text-center py-1 mx-2 text-xl italic text-white tracking-widest">
-          In Progress
-        </h1>
-        {todos.progress?.map((todo) => (
-          <Task
-            back="todo"
-            next="review"
-            key={todo._id}
-            data={todo}
-            fetchData={fetchData}
-          />
-        ))}
-      </section>
-      <section className="w-[300px] my-5 mx-2">
-        <h1 className="font-bold bg-orange-500 rounded-md text-center py-1 mx-2 text-xl italic text-white tracking-widest">
-          Review
-        </h1>
-        {todos.review?.map((todo) => (
-          <Task
-            back="progress"
-            next="done"
-            key={todo._id}
-            data={todo}
-            fetchData={fetchData}
-          />
-        ))}
-      </section>
-      <section className="w-[300px] my-5 mx-2">
-        <h1 className="font-bold bg-green-700 rounded-md text-center py-1 mx-2 text-xl italic text-white tracking-widest">
-          Done
-        </h1>
-        {todos.done?.map((todo) => (
-          <Task
-            back="review"
-            data={todo}
-            key={todo._id}
-            fetchData={fetchData}
-          />
-        ))}
-      </section>
+      {Object.keys(todos).length === 0 ? (
+        <Loading text="Loading Todos..." />
+      ) : (
+        <>
+          <section className="w-[300px] my-5 mx-2">
+            <h1 className="font-bold bg-yellow-500 rounded-md text-center py-1 mx-2 text-xl italic text-white tracking-widest">
+              Todo
+            </h1>
+            {todos.todo?.map((todo) => (
+              <Task
+                next="progress"
+                key={todo._id}
+                data={todo}
+                fetchData={fetchData}
+              />
+            ))}
+          </section>
+          <section className="w-[300px] my-5 mx-2">
+            <h1 className="font-bold bg-purple-600 rounded-md text-center py-1 mx-2 text-xl italic text-white tracking-widest">
+              In Progress
+            </h1>
+            {todos.progress?.map((todo) => (
+              <Task
+                back="todo"
+                next="review"
+                key={todo._id}
+                data={todo}
+                fetchData={fetchData}
+              />
+            ))}
+          </section>
+          <section className="w-[300px] my-5 mx-2">
+            <h1 className="font-bold bg-orange-500 rounded-md text-center py-1 mx-2 text-xl italic text-white tracking-widest">
+              Review
+            </h1>
+            {todos.review?.map((todo) => (
+              <Task
+                back="progress"
+                next="done"
+                key={todo._id}
+                data={todo}
+                fetchData={fetchData}
+              />
+            ))}
+          </section>
+          <section className="w-[300px] my-5 mx-2">
+            <h1 className="font-bold bg-green-700 rounded-md text-center py-1 mx-2 text-xl italic text-white tracking-widest">
+              Done
+            </h1>
+            {todos.done?.map((todo) => (
+              <Task
+                back="review"
+                data={todo}
+                key={todo._id}
+                fetchData={fetchData}
+              />
+            ))}
+          </section>
+        </>
+      )}
     </div>
   );
 }
